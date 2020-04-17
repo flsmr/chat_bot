c7bdc6529dfa2a6329c1afa47ce31427ad9f443f
flsmr <keepmeupdated@gmx.de>
2020-04-17 20:14:35 +0000

    added constructors in chatbot.h

--- src/chatbot.h
+++ src/chatbot.h
@@ -3,6 +3,7 @@
 
 #include <wx/bitmap.h>
 #include <string>
+#include <iostream>
 
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
@@ -29,7 +30,71 @@ public:
 
     //// STUDENT CODE
     ////
+    // Copy Constructor 
+    ChatBot (const ChatBot &source_chatbot) 
+    {
+        std::cout << "ChatBot Copy Constructor" << std::endl;
 
+        _chatLogic = source_chatbot._chatLogic;
+        //_chatLogic->SetChatbotHandle(this);
+
+        _rootNode = source_chatbot._rootNode;
+
+        //_image = new wxBitmap();  
+        _image = source_chatbot._image;
+
+    }
+    // Copy Assign Constructor
+    ChatBot &operator=(const ChatBot &source_chatbot)
+    {
+        std::cout << "ChatBot Copy Assign Constructor" << std::endl;
+
+        if (this == &source_chatbot)
+            return *this;
+        
+        _chatLogic = source_chatbot._chatLogic;
+        _rootNode = source_chatbot._rootNode;
+        _image = source_chatbot._image;
+
+        return *this;
+    }
+
+    // Move Constructor 
+    ChatBot(ChatBot &&source_chatbot)
+    {
+        std::cout << "ChatBot Move Constructor" << std::endl;
+
+        _chatLogic = source_chatbot._chatLogic;
+        _rootNode = source_chatbot._rootNode;
+        _image = source_chatbot._image;
+
+        // clean up source
+        source_chatbot._chatLogic = nullptr;
+        source_chatbot._rootNode = nullptr; 
+        delete source_chatbot._image;
+        source_chatbot._image = NULL;
+    }
+
+    // Move Assign Constructor 
+    ChatBot &operator=(ChatBot &&source_chatbot)
+    {
+        std::cout << "ChatBot Move Assignment Constructor" << std::endl;
+
+        if (this == &source_chatbot)
+            return *this;
+
+        _chatLogic = source_chatbot._chatLogic;
+        _rootNode = source_chatbot._rootNode;
+        _image = source_chatbot._image;
+
+        // clean up source
+        source_chatbot._chatLogic = nullptr;
+        source_chatbot._rootNode = nullptr; 
+        delete source_chatbot._image;
+        source_chatbot._image = NULL;
+
+        return *this;
+    }    
     ////
     //// EOF STUDENT CODE
 
